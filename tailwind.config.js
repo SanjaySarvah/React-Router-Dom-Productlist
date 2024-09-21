12619798c46ca9ed64592e2f43b48e9e7d3d4a07
SanjaySarvah <110657464+SanjaySarvah@users.noreply.github.com>
2024-09-21 23:22:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: { animation: {
+      marquee: 'marquee 5s linear infinite',
+    },
+    keyframes: {
+      marquee: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(-100%)' },
+      },
+    },},
+  },
+  plugins: [],
+}
+
